microshift-io/microshift

Configurable FROM for the runner

Makefile
@@ -28,7 +28,7 @@ else
 OKD_RELEASE_IMAGE ?= quay.io/okd/scos-release
 endif
 
-BUILDER_IMAGE := microshift-okd-builder
+BUILDER_IMAGE ?= microshift-okd-builder
 USHIFT_IMAGE := microshift-okd
 LVM_DISK := /var/lib/microshift-okd/lvmdisk.image
 VG_NAME := myvg1
@@ -106,8 +106,8 @@ rpm-to-deb:
 
 .PHONY: image
 image:
-	@if ! sudo podman image exists microshift-okd-builder ; then \
-		echo "ERROR: Run 'make rpm' to build the MicroShift RPMs" ; \
+	@if ! sudo podman image exists "${BUILDER_IMAGE}" ; then \
+		echo "ERROR: Run 'make rpm' or 'make rpm-copr' to build the MicroShift RPMs" ; \
 		exit 1 ; \
 	fi
 
@@ -119,6 +119,7 @@ image:
         --label okd.version="${OKD_VERSION_TAG}" \
         --build-arg BOOTC_IMAGE_URL="${BOOTC_IMAGE_URL}" \
         --build-arg BOOTC_IMAGE_TAG="${BOOTC_IMAGE_TAG}" \
+        --build-arg RPM_BUILDER_IMAGE="${BUILDER_IMAGE}" \
     	--env WITH_KINDNET="${WITH_KINDNET}" \
     	--env WITH_TOPOLVM="${WITH_TOPOLVM}" \
     	--env WITH_OLM="${WITH_OLM}" \

pmtk/microshift-io

Makefile
@@ -38,7 +38,7 @@ else
 OKD_RELEASE_IMAGE ?= $(OKD_RELEASE_IMAGE_X86_64)
 endif
 
-RPM_IMAGE := microshift-okd-rpm
+RPM_IMAGE ?= microshift-okd-rpm
 USHIFT_IMAGE := microshift-okd
 SRPM_IMAGE := microshift-okd-srpm
 LVM_DISK := /var/lib/microshift-okd/lvmdisk.image
@@ -118,7 +118,7 @@ rpm-to-deb:
 .PHONY: image
 image:
 	@if ! sudo podman image exists "${RPM_IMAGE}" ; then \
-		echo "ERROR: Run 'make rpm' to build the MicroShift RPMs" ; \
+		echo "ERROR: Run 'make rpm' or 'make rpm-copr' to build the MicroShift RPMs" ; \
 		exit 1 ; \
 	fi
 
@@ -130,6 +130,7 @@ image:
         --label okd.version="${OKD_VERSION_TAG}" \
         --build-arg BOOTC_IMAGE_URL="${BOOTC_IMAGE_URL}" \
         --build-arg BOOTC_IMAGE_TAG="${BOOTC_IMAGE_TAG}" \
+        --build-arg RPM_IMAGE="${RPM_IMAGE}" \
     	--env WITH_KINDNET="${WITH_KINDNET}" \
     	--env WITH_TOPOLVM="${WITH_TOPOLVM}" \
     	--env WITH_OLM="${WITH_OLM}" \